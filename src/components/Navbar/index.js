651a977a8818449c3d194911b587c1e746265df7
JDutra <dutraneto@me.com>
2020-06-18 14:43:08 -0400

    Finish adding styles to  Navbar component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,7 +5,21 @@ import * as S from './styled'
 function Navbar() {
     return (
         <S.NavbarWrapper>
-            <S.Navbar></S.Navbar>
+            <S.Navbar>
+                <S.NavbarList>
+                    <S.NavbarItem>
+                        <S.NavbarItemLink>Minha conta</S.NavbarItemLink>
+                    </S.NavbarItem>
+                    <S.NavbarItem>
+                        <S.NavbarItemLink>Pré-matrículas</S.NavbarItemLink>
+                    </S.NavbarItem>
+                    <S.NavbarItem>
+                        <S.NavbarItemLink href='/' className='active'>
+                            Bolsas favoritas
+                        </S.NavbarItemLink>
+                    </S.NavbarItem>
+                </S.NavbarList>
+            </S.Navbar>
         </S.NavbarWrapper>
     )
 }
--- src/components/Navbar/styled.js
+++ src/components/Navbar/styled.js
@@ -2,12 +2,38 @@ import styled from 'styled-components'
 
 export const NavbarWrapper = styled.div`
     background: var(--blue-primary);
-    padding: 1rem 0;
 `
 
 export const Navbar = styled.nav`
     max-width: 1440px;
     margin: 0 auto;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.3);
+`
+
+export const NavbarList = styled.ul`
     display: flex;
     justify-items: flex-start;
+`
+
+export const NavbarItem = styled.li`
+    color: #fff;
+    font-size: 1rem;
+    height: 100%;
+    font-weight: bold;
+    cursor: pointer;
+
+    .active {
+        background-color: var(--blue-secondary);
+    }
+`
+
+export const NavbarItemLink = styled.a`
+    display: inline-block;
+    padding: 1rem 2.25rem;
+    transition: background-color 0.3s;
+    &:hover {
+        background-color: var(--blue-secondary);
+    }
 `
